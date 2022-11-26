56de680d814b29fefe9848e2ae815ea874cb2a6a
Muhammad Ashhad Alam <ashhad13@mit.edu>
2022-11-26 18:49:40 -0500

    creating matrix with just player position ratings

--- extracting_liverpool_data.py
+++ extracting_liverpool_data.py
@@ -3,7 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 
 df = pd.read_csv('all_data/players_22.csv')
-liv_val = df.loc[df['club_name'] == 'Liverpool']
+liv_val_inter = df.loc[df['club_name'] == 'Liverpool']
+player_pos = ["ls", "st", "rs", "lw", "lf", "cf", "rf", "rw", "lam", "cam", "ram", "lm", "lcm", "cm", "rcm", \
+                "rm", "lwb", "ldm", "cdm", "rdm", "rwb", "lb", "lcb", "cb", "rcb", "rb", "gk"]
+player_info = ["short_name", "player_positions", "overall", "age"] + player_pos
+liv_val = liv_val_inter[player_info]
+for pos in player_pos:
+    liv_val[pos] = liv_val[pos].apply(lambda x: eval(x))
 liv_val.to_csv('Liverpool_FIFA_22.csv')
 
 url = 'https://www.fifaindex.com/teams/fifa22/?league=13'
